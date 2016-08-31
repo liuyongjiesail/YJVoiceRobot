9298359bb825767b2b1bc599a281952fb9884aef
刘永杰 <liuyongjiesail@icloud.com>
2016-08-31 11:15:19 +0800

    智能语音机器人

---
+++ VoiceRobotQ/VoiceRobotQ/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VoiceRobotQ
+//
+//  Created by 刘永杰 on 15/12/28.
+//  Copyright © 2015年 sail. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
